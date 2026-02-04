304e78f876ace1df0f52fa82e28e440ca9c5b999
samiul alom sium <siumdeveloper@gmail.com>
2026-02-04 18:23:16 +0600

    Fix: Map root URL to product.html

---
+++ productWeb/src/main/java/org/microservice/productweb/config/WebConfig.java
@@ -0,0 +1,15 @@
+package org.microservice.productweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward the root path to product.html
+        registry.addViewController("/").setViewName("forward:/product.html");
+    }
+}
